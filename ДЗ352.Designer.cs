a568eb21e0218f6a2f1165077d9d22c7aa29a4d6
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2024-07-01 10:40:01 +0300

    Add files via upload

---
+++ ДЗ352.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer;
+        private int currentPointIndex;
+        private System.Drawing.Point[] linePoints;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+
+         
+            this.startButton.Location = new System.Drawing.Point(10, 10);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+
+           
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+        }
+
+        private void startButton_Click(object sender, System.EventArgs e)
+        {
+            currentPointIndex = 0;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, System.EventArgs e)
+        {
+            if (currentPointIndex < linePoints.Length - 1)
+            {
+                currentPointIndex++;
+                this.Invalidate(); 
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+
+        private void Form1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            if (currentPointIndex > 0)
+            {
+                e.Graphics.DrawLines(System.Drawing.Pens.Black, linePoints[..(currentPointIndex + 1)]);
+            }
+        }
+
+        private System.Drawing.Point[] GenerateWavePoints(int width, int height, int amplitude, int frequency)
+        {
+            System.Drawing.Point[] points = new System.Drawing.Point[width];
+            for (int i = 0; i < width; i++)
+            {
+                int y = (int)(height / 2 + amplitude * System.Math.Sin(2 * System.Math.PI * frequency * i / width));
+                points[i] = new System.Drawing.Point(i, y);
+            }
+            return points;
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            linePoints = GenerateWavePoints(800, 100, 10, 50);
+        }
+    }
+}
+
---
+++ ДЗ352.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public class MainForm : Form
+    {
+        private Button startButton;
+        private Timer timer;
+        private int currentPointIndex;
+        private Point[] linePoints;
+
+        public MainForm()
+        {
+            startButton = new Button();
+            startButton.Text = "Start";
+            startButton.Location = new Point(10, 10);
+            startButton.Click += StartButton_Click;
+
+            timer = new Timer();
+            timer.Interval = 50; // Интервал 50 миллисекунд
+            timer.Tick += Timer_Tick;
+
+            this.Controls.Add(startButton);
+            this.Paint += MainForm_Paint;
+            this.Size = new Size(800, 600);
+
+            // Пример точки для рисования волнистой линии
+            linePoints = GenerateWavePoints(800, 100, 10, 50);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            currentPointIndex = 0;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (currentPointIndex < linePoints.Length - 1)
+            {
+                currentPointIndex++;
+                this.Invalidate(); // Перерисовываем форму
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentPointIndex > 0)
+            {
+                e.Graphics.DrawLines(Pens.Black, linePoints[..(currentPointIndex + 1)]);
+            }
+        }
+
+        private Point[] GenerateWavePoints(int width, int height, int amplitude, int frequency)
+        {
+            Point[] points = new Point[width];
+            for (int i = 0; i < width; i++)
+            {
+                int y = (int)(height / 2 + amplitude * Math.Sin(2 * Math.PI * frequency * i / width));
+                points[i] = new Point(i, y);
+            }
+            return points;
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new MainForm());
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(571, 436);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
